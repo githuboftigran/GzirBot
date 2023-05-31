63ca6da82a9961687955398684949ea3f977dd16
tigran <mail.of.tigran@gmail.com>
2023-05-31 17:20:57 +0400

    * Help text updated  * Help text will be sent for /start command as well

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import re
 UPDATES_POLLING_TIMEOUT = 60
 UPDATES_FAILURE_TIMEOUT = 10
 
-UPDATE_INTERRUPTIONS_INTERVAL = 60 * 60
+UPDATE_INTERRUPTIONS_INTERVAL = 5 * 60
 
 DAY_SECONDS = 60 * 60 * 24
 INTERRUPTION_LIFESPAN = DAY_SECONDS
@@ -11,10 +11,13 @@ INTERRUPTION_LIFESPAN = DAY_SECONDS
 WHITESPACES_PATTERN = re.compile(r'\s+')
 
 HELP_TEXT = """
-Here are the following commands available:
+GzirBot scrapes veolias site every 5 minutes to get information about water interruption announcements.
+Announcement text will be forwarded to you if it contains any of your specified keywords.
+The bot is, however, not smart enough and if you add a street name, but it's conjugated in the announcement, the bot won't notify you.
+E.g. announcements with words ուլնեցու, լոռվա, won't be forwarded if your keywords are ուլնեցի, լոռի, so you can add keywords ուլնեց and լոռ instead.
+To add or remove keywords, please don't tap on /add or /remove links. Type manually and send as message. E.g. /add Նալբանդյան
 
-/help
-Show help menu
+Here is the list of available commands:
 
 /add keywords
 Keywords should be separated by commas.
@@ -27,10 +30,21 @@ Example: `/add Նալբանդյան, Ա.Ահարոնյան, Փարպեցի`
 /show
 This will show current keywords you are subscribed to.
 
-All the data is taken from: https://interactive.vjur.am/
+/help
+Show help text
+
+All the announcements data is scraped from: https://interactive.vjur.am
 """
 
 UNKNOWN_COMMAND_TEXT = f'Unknown command.\n{HELP_TEXT}'
+
+NO_KEYWORDS_TEXT = """No keywords are specified.
+
+Please don't tap on the command in bots command list.
+
+Type manually and send as message. E.g.
+ /add կոմիտաս, ազատությ
+ """
 
 SHOW_KEYWORDS_TEXT = """
 Here are the keywords you are subscribed to:
--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from telegram import start_receiving_updates
 from telegram.api import send_message
 
 from users import init_users, notify_user, notify_all, add_keywords, remove_keywords, get_keywords
-from constants import UNKNOWN_COMMAND_TEXT, SHOW_KEYWORDS_TEXT, KEYWORDS_ADDED_TEXT, KEYWORDS_REMOVED_TEXT, HELP_TEXT, NO_KEYWORDS_TEXT
+from constants import \
+    UNKNOWN_COMMAND_TEXT,\
+    SHOW_KEYWORDS_TEXT,\
+    KEYWORDS_ADDED_TEXT,\
+    KEYWORDS_REMOVED_TEXT,\
+    HELP_TEXT,\
+    NO_KEYWORDS_TEXT
 from scraping import interruptions, start_scraping
 
 
@@ -21,6 +27,9 @@ def handle_telegram_updates(updates):
         text = message['text'].strip()
         if text.startswith('/add'):
             keywords = text[len('/add'):].strip().split(',')
+            if not keywords:
+                send_message(user_id, NO_KEYWORDS_TEXT)
+                return
             keywords = [k.strip().lower() for k in keywords]
             add_keywords(user_id, keywords)
             current_keywords_text = SHOW_KEYWORDS_TEXT.format(', '.join(get_keywords(user_id)))
@@ -28,11 +37,14 @@ def handle_telegram_updates(updates):
             notify_user(user_id, interruptions.values())
         elif text.startswith('/remove'):
             keywords = text[len('/remove'):].strip().split(',')
+            if not keywords:
+                send_message(user_id, NO_KEYWORDS_TEXT)
+                return
             keywords = [k.strip().lower() for k in keywords]
             remove_keywords(user_id, keywords)
             current_keywords_text = SHOW_KEYWORDS_TEXT.format(', '.join(get_keywords(user_id)))
             send_message(user_id, f"{KEYWORDS_REMOVED_TEXT}\n\n{current_keywords_text}")
-        elif text.startswith('/help'):
+        elif text.startswith('/help') or text.startswith('/start'):
             send_message(user_id, HELP_TEXT)
         elif text.startswith('/show'):
             keywords = get_keywords(user_id)
